003a816758ae2fc48d6d01055b70c91d2e4146fe
ConnerJack <130625816+ConnerJack@users.noreply.github.com>
2023-06-27 10:28:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
+	str = str.split(" ").join("")
+	let ans = ""
+	for (let index = str.length; index >= 0 ; index--) {
+		ans = ans + str.charAt(index)
+	}
+
+	if (str == ans) {
+		return true
+	} else {
+		return false
+	}
 
 }
 module.exports = palindrome
